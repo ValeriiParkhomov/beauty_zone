d1e991bfcc78377fdc3b6671ec1b22ac25b50ef9
ValeriiParkhomov <149947169+ValeriiParkhomov@users.noreply.github.com>
2023-12-13 15:40:06 +0200

    fix mobile-menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,19 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
+const about = document.querySelector('.about');
+const advantages = document.querySelector('.advantages');
+const procedures = document.querySelector('.procedures');
+const contacts = document.querySelector('.contacts');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+
+about.addEventListener('click', toggleMenu);
+advantages.addEventListener('click', toggleMenu);
+procedures.addEventListener('click', toggleMenu);
+contacts.addEventListener('click', toggleMenu);
+
+
